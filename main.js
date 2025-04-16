36f26a6ce988aa6753dbffece457b9be243d5283
Thimer01 <thiago.merce.2016@gmail.com>
2025-04-16 16:56:03 -0300

    borrar

--- main.js
+++ main.js
@@ -4,20 +4,44 @@ const mas = document.getElementById("agragar");
 
 const lista = document.getElementById("Thi_ul");
 
-const agragarTarea = (tarea) => {
+const hecho = 'fa-check-circle';
+
+const pendiente = 'fa-circle';
+
+const tachado = 'tachado'
+
+const agragarTarea = (tarea, check) => {
+
+    const estado = check? hecho : pendiente;
+
+    const tachar = check? tachado : "";
 
     const elemento = `<li class="Thi_li" id="Thi_li">
                     
-                    <i class="fas fa-circle co" id="check" data-="check"></i>
+                    <i class="far ${estado} check" id="check" data="check"></i>
                     
-                    <p class="tarea tachado" id="tarea"> ${tarea} </p>
+                    <p class="tarea ${tachado}" id="tarea"> ${tarea} </p>
                     
-                    <i class="fas fa-trash de" id="borrar" data-="borrar"></i>
+                    <i class="fas fa-trash de" id="borrar" data="borrar"></i>
                 
                 </li>`
 
                 lista.insertAdjacentHTML("beforeend", elemento);
 };
+
+const tareaRealizada = (element) => {
+
+    element.classList.toggle(hecho);
+
+    element.classList.toggle(pendiente);
+
+    element.parentNode.querySelector('.tarea').classList.toggle(tachado);
+};
+
+const tareaEliminada = (element) => {
+
+    element.parentNode.parentNode.removeChild(element.parentNode);
+}
 
 const cambiarEstilo = () => {
 
@@ -25,7 +49,9 @@ const cambiarEstilo = () => {
 
     link.href = link.href.includes("style.css")? "style2.css" : "style.css";
 };
+
 //LLAMAR FUNCION POR CLICK
+
 
 mas.addEventListener('click', () => {
 
@@ -60,3 +86,21 @@ document.addEventListener('keyup', (e) => {
     
 });
 
+lista.addEventListener('click', function(event) {
+
+    const element = event.target;
+
+    const elementData = element.atributes.data.value;
+
+    if(elementData == 'check'){
+        
+        tareaRealizada(element)
+
+    }
+
+    else if(elementData == 'borrar'){
+
+        tareaEliminada(element)
+
+    }
+});
